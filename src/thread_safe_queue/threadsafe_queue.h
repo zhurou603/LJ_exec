2f0d7ec17720bc9c29694b0bd79187a6be6edd30
zhurou603 <526590351@qq.com>
2019-08-13 17:22:14 +0800

    rename class name

--- src/thread_safe_queue/threadsafe_queue.h
+++ src/thread_safe_queue/threadsafe_queue.h
@@ -6,11 +6,11 @@
 using namespace std;
 
 template<typename T>
-class threadsafe_queue{
+class ThreadsafeQueue{
  public:
-	threadsafe_queue(){};
-	threadsafe_queue(const threadsafe_queue&);
-	threadsafe_queue& operator=(const threadsafe_queue&) = delete;
+	ThreadsafeQueue(){};
+	ThreadsafeQueue(const ThreadsafeQueue&);
+	ThreadsafeQueue& operator=(const ThreadsafeQueue&) = delete;
 	void push(T new_value);
 	shared_ptr<T> try_pop();
 	shared_ptr<T> wait_and_pop();
